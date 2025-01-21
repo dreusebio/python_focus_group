260c0b728558c3e6fd1715a91952e1e696d01db0
dreusebio <83031200+dreusebio@users.noreply.github.com>
2025-01-20 22:33:40 -0800

    session_04 adding comments

---
+++ session_04/comments.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+print ("Here is an example  of an inline comment") # inline comment
+
+#here is an example of single comments
+print (" i have put a single comment above this and this is what I will use for coding")
+
+#below will be an example of a multi-line comment/block
+'''
+Script usage:
+To run the comments.py you should use the command "python3 comments.py" at the command line.
+
+Output:
+The output of comments.py are statement used arguements for illustrative purposes only
+'''
+''' 
+#print numbers 1-20
+for i in range(1,21):
+    print(i)
+
+'''
+
+#print names of all pets
+pet_names = ["Steella", "Bleue", "sunny", "New Pet"]
+for pet in pet_names:
+    print(pet)
